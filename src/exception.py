e60e651c66fef0d097f94dc273b78322a362963b
Bishal <bishalpandey102@gmail.com>
2026-01-05 13:52:46 +0545

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
+from src.logger import logging
 
-
-def error_detail(error, error_detail:sys):
+def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_no = exc_tb.tb_lineno
@@ -9,9 +9,11 @@ def error_detail(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
+
     def __init__(self, error_messsage, error_detail:sys):
-        super().init__(error_messsage)
-        self.error_message = error_detail(error_messsage, error_detail=error_detail)
+        super().__init__(error_messsage)
+        self.error_message = error_message_detail(error_messsage, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
