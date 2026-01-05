85d715a34de971d95e71cec7bc23671d561b5b3c
Bishal <bishalpandey102@gmail.com>
2026-01-05 17:24:52 +0545

    Application

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler 
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_ingestion import DataIngestion
